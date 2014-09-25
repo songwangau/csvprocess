0726d1e003d3e60e3b999ebcb66156c08a23772e
ws72 <song.wang.au@gmail.com>
2014-09-26 00:01:08 +1000

    email validation, user name and surname format.

--- csvprocess/src/csv_proc.php
+++ csvprocess/src/csv_proc.php
@@ -7,10 +7,23 @@ $file = fopen("users.csv","r");
 while(! feof($file))
 {
 	$arr = fgetcsv($file);
-	if($arr[0]!="")
-		$dao->addUser($arr[0],$arr[1],$arr[2]);
+	$dao->addUser(capfirst($arr[0]),capfirst($arr[1]),emailValidate($arr[2]));
 }
 
 fclose($file);
 
+function capfirst($bar){
+	return ucfirst(strtolower(trim($bar)));
+}
+
+function emailValidate($email){
+	$email = trim($email);
+	if (preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/',$email)) {
+		$email = strtolower($email);
+	} else {
+		$email="";
+		error_log("You messed up!", 3, "my-errors.log");
+	}
+	return $email;
+}
 ?>
